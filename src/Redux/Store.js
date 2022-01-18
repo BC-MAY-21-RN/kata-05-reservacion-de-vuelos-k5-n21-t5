f364365905ecac9ecb00a64a49ad35abf3db53b4
HugoCamposLopez <hugo.antonio.campos@gmail.com>
2022-01-18 17:15:09 -0600

    Redux and booking working.
    Co-authored-by: GerardoIbarra <libarra2@ucol.mx>
    Co-authored-by: OsvaldoMDLM <osvaldo.benjamin31@gmail.com>

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -5,4 +5,3 @@ import userReducer from './Reducers';
 const rootReducer = combineReducers({userReducer});
 
 export const Store = createStore(rootReducer, applyMiddleware(thunk));
-Store.subscribe(() => console.log('Cambio de estado ', Store.getState()));
